20acd81433cb6100b976eff8459cc59d15e9e5a8
Brad <brad@bradeagle.com>
2025-12-09 19:55:39 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 use std::{env, io};
 use crate::commands::general::command;
 
-//record create -r a -c in -name uncentralized.unet -address 127.0.0.1 -ttl 300 -local true
+//record create -r a -c in -name find9.net -address 127.0.0.1 -ttl 300 -local true
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
